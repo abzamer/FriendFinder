f5282035290e844a8d0197b9a32a12e56f3b34b8
Angelica Zamer <angelica.zamer@gmail.com>
2019-10-14 15:57:57 -0500

    initial layout of site set up. Favicon needed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,7 @@
-var express = require("express");
-var path = require("path");
+//this file should have the following
+//all of the app's data as an array of objects. refer to hw doc to see format
+
+
+
 
 
