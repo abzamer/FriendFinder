daf05cfafbf204c0828099a10e5b79e80a03272f
Angelica Zamer <angelica.zamer@gmail.com>
2019-10-14 22:28:33 -0500

    form info being recorded; needs to be pushed to array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,6 +9,9 @@ app.get("/api/friends", function(req, res){
 
 //A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 // app.post("/api/friends", function(req, res) {
+//     console.log("I'm in the friends post!");
 
+//     console.log("here's what i have: " + req.)
+//     res.json(friendsData.friendsArray);
 // })
 }
