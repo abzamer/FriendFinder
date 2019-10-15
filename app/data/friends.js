4b0339e1e0290f6f675c72da4f0321633407d83f
Angelica Zamer <angelica.zamer@gmail.com>
2019-10-14 21:14:27 -0500

    friendsArray setup with hardcoded data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,21 @@
 //this file should have the following
 //all of the app's data as an array of objects. refer to hw doc to see format
 
+var friendsArray = [
+    {
+        name: "Homer Simpson",
+        photo: "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
+        scores: [
+            1,
+            3,
+            1,
+            5,
+            5
+        ]
+    }
+];
 
+module.exports = friendsArray;
 
 
 
